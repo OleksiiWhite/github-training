f3f462269dd6d011ce5e4a0768d195b3ae93fbe2
Oleksii Biletskyi <alenikbel@ukr.net>
2022-07-11 11:33:27 +0300

    training js

--- js/training.js
+++ js/training.js
@@ -1,12 +1,21 @@
+function makeTransaction(5, 10, 40);
+
 function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
   let message;
   // Change code below this line
   const totalPrice = pricePerDroid * orderedQuantity;
-  if (customerCredits <= totalPrice) {
+  if (customerCredits < totalPrice) {
     message = 'Insufficient funds!';
   } else {
     message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
   }
   // Change code above this line
   return message;
+}
+
+
+function checkIfCanAccessContent(subType) {
+  const canAccessContent = subType === "pro" || subType === "vip"; // Change this line
+
+  return canAccessContent;
 }
