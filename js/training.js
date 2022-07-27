5ca45f0529a22221dc3423a501cbc4f1a225d06c
Oleksii Biletskyi <alenikbel@ukr.net>
2022-07-27 16:33:57 +0300

    treining js

--- js/training.js
+++ js/training.js
@@ -1,12 +1,24 @@
-function slugify(title) {
-  // Change code below this line
-  let slug;
-  slug = title.split(' ');
-  slug = slug.join('-');
-  slug = slug.toLowerCase();
-  console.log(slug);
-  return slug;
+// function slugify(title) {
+//   // Change code below this line
+//   let slug;
+//   slug = title.split(' ');
+//   slug = slug.join('-');
+//   slug = slug.toLowerCase();
+//   console.log(slug);
+//   return slug;
 
+//   // Change code above this line
+// }
+// slugify('How to become a JUNIOR developer in TWO WEEKS');
+
+function makeArray(firstArray, secondArray, maxLength) {
+  // Change code below this line
+  let allArray = firstArray.concat(secondArray);
+  const longArray = allArray.length;
+  if (longArray > maxLength) {
+    return allArray.slice(0, maxLength);
+  }
+  return allArray;
   // Change code above this line
 }
-slugify('How to become a JUNIOR developer in TWO WEEKS');
+makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4);
