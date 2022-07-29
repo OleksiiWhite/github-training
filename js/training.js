4e832bd7bbe572d31fc384dcde22d7bf21fd38e2
Oleksii Biletskyi <alenikbel@ukr.net>
2022-07-29 15:27:02 +0300

    training js

--- js/training.js
+++ js/training.js
@@ -54,14 +54,69 @@
 // findLongestWord('May the force be with you');
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-function createArrayOfNumbers(min, max) {
-  const numbers = [];
-  // Change code below this line
-  for (let i = min; i <= max + 1; i += 1) {
-    numbers.push(i);
-  }
-  // Change code above this line
-  console.table(numbers);
-  return numbers;
-}
-createArrayOfNumbers(14, 17);
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (let i = min; i <= max + 1; i += 1) {
+//     numbers.push(i);
+//   }
+//   // Change code above this line
+//   console.table(numbers);
+//   return numbers;
+// }
+// createArrayOfNumbers(14, 17);
+
+////////////////////////////////////////////////////
+
+// function filterArray(numbers, value) {
+//   const bigNumbers = [];
+//   const numbersLong = numbers.length - 1;
+
+//   for (const i = 0; i <= numbersLong; i += 1) {
+//     if (numbers[i] > value) {
+//       bigNumbers = bigNumbers.push(numbers[i]);
+//     }
+//     console.log(bigNumbers);
+//     return bigNumbers;
+//   }
+//   console.log(bigNumbers);
+//   return bigNumbers;
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3);
+
+////////////////////////////////////////////////////
+// function checkFruit(fruit) {
+//   const fruits = ['apple', 'plum', 'pear', 'orange'];
+//   fruits.includes(fruit);
+//   return;
+// }
+////////////////////////////////////////
+
+// function getCommonElements(array1, array2) {
+//   // Change code below this line
+//   const twoArray = [];
+//   for (let i = 0; i < array1.length; i += 1) {
+//     if (array2.includes(array1[i])) {
+//       twoArray.push(array1[i]);
+//     }
+//   }
+//   console.log(twoArray);
+//   return twoArray;
+
+//   // Change code above this line
+// }
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]);
+///////////////////////////////////////////////////////////
+
+// function getEvenNumbers(start, end) {
+//   const twoAll = [];
+//   for (i = start; i <= end; i+=1){
+//     if (i % 2 === 0){
+//       twoAll.push(i);
+//     }
+//   }
+//   return twoAll;
+//   }
+
+////////////////////////////////////////////////////////
