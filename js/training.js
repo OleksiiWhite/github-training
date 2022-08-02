424d12458a0540f97227fd505d825189200a325f
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-02 17:42:06 +0300

    training  js 1-5 lessons

--- js/training.js
+++ js/training.js
@@ -135,14 +135,78 @@
 // countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 });
 
 //////////////////////////////////////////////
-const apartment = {
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-};
-const values = [];
-// Change code below this line
-const keys = Object.keys(apartment);
-for (const key of keys) {
-  values.push(apartment[key]);
-}
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+///////////////////////////////////////////////////////
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+//////////////////////////////////////////////////////////////////
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const valuesPropName = [];
+//   for (const product of products) {
+//     const { name, price, quantity } = product;
+
+//     for (const key in product) {
+//       if (propName === key) {
+//         valuesPropName.push(product[key]);
+//       }
+//     }
+//   }
+
+//   console.log(valuesPropName);
+//   return valuesPropName;
+// }
+// getAllPropValues('price');
+
+///////////////////////////////////////////
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   let totalPrice = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       const totalPriceProd = product.price * product.quantity;
+//       totalPrice += totalPriceProd;
+//       return totalPrice;
+//     }
+//   }
+// }
+// calculateTotalPrice('Droid');
+////////////////////////////////////////////
