8b2f80d3ab22be27dad20b8001a22ee3b86cb80c
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-17 20:48:45 +0300

    training js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -6,3 +6,15 @@
 // const result = makePizza();
 // const pointer = makePizza;
 // /////////////////////////////////////////////////
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName) {
+  return;
+}
