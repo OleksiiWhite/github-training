d1a89d0b884d799407f1c14956e7faab6b4a20d7
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-18 21:16:11 +0300

    add training js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -32,3 +32,17 @@
 //   return callback(pizzaName);
 // }
 ///////////////////////////////////////////////////////////////////////////////////////
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza('Royal Grand', function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza('Ultracheese', function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+///////////////////////////////////////////////////////
