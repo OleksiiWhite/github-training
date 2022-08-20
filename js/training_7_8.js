066b8d11e8aaab66ab06f0243c111d1cb6c85969
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-20 13:28:26 +0300

    add js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -203,3 +203,26 @@
 // const evenNumbers = numbers.filter(number => number % 2 === 0);
 // const oddNumbers = numbers.filter(number => number % 2 !== 0);
 ////////////////////////////////////////////////////////////////////
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction', 'mysticism'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism', 'adventure'],
+//   },
+// ];
+// // Change code below this line
+// const allGenres = books.flatMap(book => book.genres);
+// const uniqueGenres = allGenres.filter(
+//   (course, index, array) => array.indexOf(course) === index
+// );
+////////////////////////////////////////////////////////////////////
