261ec51cc8326f75189250571492b5726b9bd275
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-20 12:52:39 +0300

    add js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -144,3 +144,26 @@
 // }
 // changeEven([2, 8, 3, 7, 4, 6], 10);
 ///////////////////////////////////////////////////
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
+//////////////////////////////////////////////////////////
