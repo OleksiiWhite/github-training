f277db4ccfa6abddcee51f412ece24c0c00dacfa
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-21 13:55:59 +0300

    add js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -413,3 +413,16 @@
 // const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
 
 ////////////////////////////////////////////////////////////////////////////
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+//   'Howard Lovecraft',
+// ];
+// // Change code below this line
+
+// const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+// const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+///////////////////////////////////////////////////////////////////////////////////
