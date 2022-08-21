fbad747d160ef8f22d5ea98e7944dc22eda87d38
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-21 13:52:49 +0300

    add js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -406,3 +406,10 @@
 //   return users.reduce((acc, user) => [...acc, ...user.friends], []).length;
 // };
 ////////////////////////////////////////////////////////////////////////////////
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+////////////////////////////////////////////////////////////////////////////
