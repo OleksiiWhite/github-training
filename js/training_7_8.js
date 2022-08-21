626be98c845b09ac78cb81437ef92e4ac52f27cd
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-21 13:30:26 +0300

    add js

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -389,3 +389,15 @@
 // // Change code above this line
 // const averagePlayTime = totalPlayTime / playtimes.length;
 ////////////////////////////////////////////////////////////////////////
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+//   return total + player.playtime / player.gamesPlayed;
+// }, 0);
+////////////////////////////////////////////////////////////////////////////////
