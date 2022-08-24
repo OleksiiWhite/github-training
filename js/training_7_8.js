cf62e0e04a24345a4a21b4df5e9e1089c066f37b
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-24 19:51:45 +0300

    add js 8

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -1,8 +1,3 @@
-// function makePizza() {
-//   return 'Your pizza is being prepared, please wait.';
-// }
-// // Change code below this line
-
 // const result = makePizza();
 // const pointer = makePizza;
 // /////////////////////////////////////////////////
