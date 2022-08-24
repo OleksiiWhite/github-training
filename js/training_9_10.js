9dc3fb04828748d68f0f263f277458f015d5ad40
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-24 19:56:56 +0300

    add js 9

---
+++ js/training_9_10.js
@@ -0,0 +1,17 @@
+const pizzaPalace = {
+  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
