438c50c0c577bc2b8f1881553cd362bafce34bcf
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-27 17:12:38 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -173,3 +173,20 @@
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
 //////////////////////////////////////////////////////////////////
+// class Car {
+//   // Change code below this line
+//   #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   getBrand() {
+//     return this.#brand;
+//   }
+//   changeBrand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+// Change code above this line
+/////////////////////////////////////////////////////////////
