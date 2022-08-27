82c1657584eedb0d7da4c566bda93ed5d581e9d6
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-27 16:15:20 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -72,3 +72,17 @@
 //   },
 //   // Change code above this line
 //////////////////////////////////////////////////////////////
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish',
+// };
+// // Change code below this line
+
+// const child = Object.create(parent);
+
+// // Change code above this line
+// child.name = 'Jason';
+// child.age = 27;
+//////////////////////////////////////////////
