0b9c80c6c33f213400ee07a60ccfb1d2a3f8e6fb
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-27 16:19:08 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -86,3 +86,20 @@
 // child.name = 'Jason';
 // child.age = 27;
 //////////////////////////////////////////////
+// const ancestor = {
+//   name: 'Paul',
+//   age: 83,
+//   surname: 'Dawson',
+//   heritage: 'Irish',
+// };
+// // Change code below this line
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+/////////////////////////////////////////////////
