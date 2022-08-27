6e266321378de0a0a3e62c704e9cf3930cecc044
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-27 17:07:51 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -150,3 +150,26 @@
 // storage.removeItem("Prolonger");
 // console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 /////////////////////////////////////////////////////////////////////////
+// class StringBuilder {
+//    constructor( initialValue ) {
+//     this.value = initialValue;
+//      }
+
+// getValue() {
+//     return this.value;
+//   };
+//  padEnd(str){this.value = this.value + str;};
+//   padStart(str){this.value = str + this.value;};
+//   padBoth(str){this.padEnd(str); this.padStart(str);};
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+//////////////////////////////////////////////////////////////////
