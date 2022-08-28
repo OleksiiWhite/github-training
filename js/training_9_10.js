f263a6bba25cbc87ea7ec72dc0efed53e94371bb
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-28 19:47:56 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -190,3 +190,30 @@
 
 // Change code above this line
 /////////////////////////////////////////////////////////////
+class Storage {
+  // Change code below this line
+
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item !== itemToRemove);
+  }
+}
+
+// Change code above this line
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
