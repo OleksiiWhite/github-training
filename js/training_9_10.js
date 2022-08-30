441f123b12adab7a79659102f128dbed1eced4e3
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-30 21:50:59 +0300

    Update training_9_10.js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -254,39 +254,39 @@
 // builder.padBoth("=");
 // console.log(builder.getValue()); // "=^.^="
 ////////////////////////////////////////////////////////////////////////////////////////////
-// class Car {
-//   // Change code below this line
-//   #brand;
+class Car {
+  // Change code below this line
+  #brand;
 
-//   constructor({ brand, model, price }) {
-//     this.#brand = brand;
-//     this.model = model;
-//     this.price = price;
-//   }
+  constructor({ brand, model, price }) {
+    this.#brand = brand;
+    this.model = model;
+    this.price = price;
+  }
 
-//   getBrand() {
-//     return this.#brand;
-//   }
+  getBrand() {
+    return this.#brand;
+  }
 
-//   changeBrand(newBrand) {
-//     this.#brand = newBrand;
-//   }
+  changeBrand(newBrand) {
+    this.#brand = newBrand;
+  }
 
-//   getModel() {
-//     return this.model;
-//   }
+  getModel() {
+    return this.model;
+  }
 
-//   updateModel(newModel) {
-//     this.model = newModel;
-//   }
+  updateModel(newModel) {
+    this.model = newModel;
+  }
 
-//   getPrice() {
-//     return this.price;
-//   }
+  getPrice() {
+    return this.price;
+  }
 
-//   setPrice(newPrice) {
-//     this.price = newPrice;
-//   }
-//   // Change code above this line
-// }
-////////////////////////////////////////////////////////////////////
+  setPrice(newPrice) {
+    this.price = newPrice;
+  }
+  // Change code above this line
+}
+//////////////////////////////////////////////////////////////////
