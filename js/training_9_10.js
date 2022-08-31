cfcede8b0da17b7e2c46a8970c1b2f901457c67c
Oleksii Biletskyi <alenikbel@ukr.net>
2022-08-31 18:12:06 +0300

    add js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -321,3 +321,21 @@
 // audi.price = 51000;
 // console.log(audi.price); // 49000
 /////////////////////////////////////////////////////////////////////
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+// static checkPrice (price){
+//   if (price > Car.#MAX_PRICE) {return "Error! Price exceeds the maximum"} else { return "Success! Price is within acceptable limits"};
+// };
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+//////////////////////////////////////////////////////////////////////////////////
