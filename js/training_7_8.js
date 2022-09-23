f92df768e0a823042a789fc05050571feac34fd7
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-23 13:23:00 +0300

    fixed task

--- js/training_7_8.js
+++ js/training_7_8.js
@@ -476,53 +476,53 @@
 //     firstName.name.localeCompare(secondName.name)
 //   );
 ////////////////////////////////////////////////////////////////////////
-// const books = [
-//   {
-//     title: 'The Last Kingdom',
-//     author: 'Bernard Cornwell',
-//     rating: 8.38,
-//   },
-//   {
-//     title: 'Beside Still Waters',
-//     author: 'Robert Sheckley',
-//     rating: 8.51,
-//   },
-//   {
-//     title: 'The Dream of a Ridiculous Man',
-//     author: 'Fyodor Dostoevsky',
-//     rating: 7.75,
-//   },
-//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-//   {
-//     title: 'The Dreams in the Witch House',
-//     author: 'Howard Lovecraft',
-//     rating: 8.67,
-//   },
-// ];
-// const MIN_BOOK_RATING = 8;
-// // Change code below this line
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  {
+    title: 'The Dreams in the Witch House',
+    author: 'Howard Lovecraft',
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+// Change code below this line
 
-// const names = [...books]
-//   .filter(book => book.rating >= MIN_BOOK_RATING)
-//   .map(book => book.author)
-//   .sort((a, b) => a.localeCompare(b));
-////////////////////////////////////////////////////////////////////
-// const getNamesSortedByFriendCount = users => {
-//   return [...users]
-//     .sort((a, b) => a.friends.length - b.friends.length)
-//     .map(users => users.name);
-// };
-///////////////////////////////////////////////////
-// const getSortedFriends = users => {
-//   return users
-//     .flatMap(user => user.friends)
-//     .filter((user, index, friends) => friends.indexOf(user) === index)
-//     .sort((a, b) => a.localeCompare(b));
-// };
-/////////////////////////////////////////////////////////////////
-// const getTotalBalanceByGender = (users, gender) => {
-//   return users
-//     .filter(user => user.gender === gender)
-//     .reduce((total, user) => total + user.balance, 0);
-// };
-/////////////////////////////////////////////////////////////////////
+const names = [...books]
+  .filter(book => book.rating >= MIN_BOOK_RATING)
+  .map(book => book.author)
+  .sort((a, b) => a.localeCompare(b));
+//////////////////////////////////////////////////////////////////
+const getNamesSortedByFriendCount = users => {
+  return [...users]
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(users => users.name);
+};
+/////////////////////////////////////////////////
+const getSortedFriends = users => {
+  return users
+    .flatMap(user => user.friends)
+    .filter((user, index, friends) => friends.indexOf(user) === index)
+    .sort((a, b) => a.localeCompare(b));
+};
+///////////////////////////////////////////////////////////////
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
+};
+///////////////////////////////////////////////////////////////////
