987cb9a304f1317743fe7156acc38837a3fb74ab
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-22 13:49:08 +0300

    Update training_9_10.js

--- js/training_9_10.js
+++ js/training_9_10.js
@@ -62,21 +62,7 @@
 // }
 
 ///////////////////////////////////////////
-// class Car {
-//   constructor({ brand, model, price }) {
-//     this.brand = brand;
-//     this.model = model;
-//     this.price = price;
-//   }
-//   getPrice() {
-//     return this.price;
-//   }
 
-//   changePrice(newPrice) {
-//     this.price = newPrice;
-//   }
-// }
-/////////////////////////////////////////////////
 // class Storage {
 //   constructor( items ) {
 //     this.items = items;
@@ -399,36 +385,36 @@
 // console.log(mango.isBlacklisted("poly@mail.com")); // true
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// function createPrewiewMarkup({ flags, name }) {
-//   return `<li class="country-list__item">
-//       <img
-//         class="country-list__flag"
-//         src="${flags.svg}"
-//         width="30px"
-//         height="20px"
-//       />
-//       <p class="country-list__name">${name.official}</p>
-//     </li>`;
-// }
+function createPrewiewMarkup({ flags, name }) {
+  return `<li class="country-list__item">
+      <img
+        class="country-list__flag"
+        src="${flags.svg}"
+        width="30px"
+        height="20px"
+      />
+      <p class="country-list__name">${name.official}</p>
+    </li>`;
+}
 
-// function createCountryInfoMurkup({
-//   flags,
-//   name,
-//   capital,
-//   population,
-//   languages,
-// }) {
-//   const langStr = Object.values(languages).join(', ');
+function createCountryInfoMurkup({
+  flags,
+  name,
+  capital,
+  population,
+  languages,
+}) {
+  const langStr = Object.values(languages).join(', ');
 
-//   return `
-//     <div class="country-list__item">
-//     <img class="country-list__flag" width="60px" height="40px" src="${flags.svg}"></img>
-//     <p class="country-list__name accent">${name.official}</p></div>
-//     <div class="description">
-//     <p class="description__name">Capital: <span class="description__second-name" >${capital}</span><p>
-//     <p class="description__name">Population: <span class="description__second-name">  ${population}</span></p>
-//     <p class="description__name">Languages: <span class="description__second-name" >${langStr}</span></p>
-//     </div>`;
-// }
+  return `
+    <div class="country-list__item">
+    <img class="country-list__flag" width="60px" height="40px" src="${flags.svg}"></img>
+    <p class="country-list__name accent">${name.official}</p></div>
+    <div class="description">
+    <p class="description__name">Capital: <span class="description__second-name" >${capital}</span><p>
+    <p class="description__name">Population: <span class="description__second-name">  ${population}</span></p>
+    <p class="description__name">Languages: <span class="description__second-name" >${langStr}</span></p>
+    </div>`;
+}
 
-// export { createPrewiewMarkup, createCountryInfoMurkup };
+export { createPrewiewMarkup, createCountryInfoMurkup };
