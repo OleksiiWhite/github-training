d6d90914636c46079b5cc62a3a71106e3815c3c9
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-24 14:50:58 +0300

    Create training11.js

---
+++ js/training11.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////
+const pizzaPalace = {
+  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+  order(pizzaName, makePizza, onOrderError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return makePizza(pizzaName);
+    }
+    return onOrderError(
+      `There is no pizza with a name ${pizzaName} in the assortment.`
+    );
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+pizzaPalace.order('Smoked', makePizza, onOrderError);
+pizzaPalace.order('Four meats', makePizza, onOrderError);
+pizzaPalace.order('Big Mike', makePizza, onOrderError);
+pizzaPalace.order('Vienna', makePizza, onOrderError);
+// //////////////////////////////////////////////////////////////////////////////
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+//   // for (let i = 0; i < orderedItems.length; i += 1) {
+//   //   totalPrice += orderedItems[i];
+//   // }
+//   orderedItems.forEach(function calculateTotalPrice(element, index, array) {
+//     totalPrice += element;
+//   });
+//   // Change code above this line
+//   console.log(totalPrice);
+//   return totalPrice;
+// }
+// calculateTotalPrice([164, 48, 291]);
