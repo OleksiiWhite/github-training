8064b70054010e2577804800cc7650ee7ab1e273
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-25 11:37:38 +0300

    Update training11.js

--- js/training11.js
+++ js/training11.js
@@ -1,36 +1,36 @@
-/////////////////////////////////////////////////////
-const pizzaPalace = {
-  pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
-  order(pizzaName, makePizza, onOrderError) {
-    if (this.pizzas.includes(pizzaName)) {
-      return makePizza(pizzaName);
-    }
-    return onOrderError(
-      `There is no pizza with a name ${pizzaName} in the assortment.`
-    );
-  },
-};
-// Change code above this line
+// /////////////////////////////////////////////////////
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, makePizza, onOrderError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return makePizza(pizzaName);
+//     }
+//     return onOrderError(
+//       `There is no pizza with a name ${pizzaName} in the assortment.`
+//     );
+//   },
+// };
+// // Change code above this line
 
-// Callback for onSuccess
-function makePizza(pizzaName) {
-  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
-}
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
 
-// Callback for onError
-function onOrderError(error) {
-  return `Error! ${error}`;
-}
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
 
-// Method calls with callbacks
-pizzaPalace.order('Smoked', makePizza, onOrderError);
-pizzaPalace.order('Four meats', makePizza, onOrderError);
-pizzaPalace.order('Big Mike', makePizza, onOrderError);
-pizzaPalace.order('Vienna', makePizza, onOrderError);
-// //////////////////////////////////////////////////////////////////////////////
-// function calculateTotalPrice(orderedItems) {
-//   let totalPrice = 0;
-//   // Change code below this line
+// // Method calls with callbacks
+// pizzaPalace.order('Smoked', makePizza, onOrderError);
+// pizzaPalace.order('Four meats', makePizza, onOrderError);
+// pizzaPalace.order('Big Mike', makePizza, onOrderError);
+// pizzaPalace.order('Vienna', makePizza, onOrderError);
+// // //////////////////////////////////////////////////////////////////////////////
+// // function calculateTotalPrice(orderedItems) {
+// //   let totalPrice = 0;
+// //   // Change code below this line
 
 //   // for (let i = 0; i < orderedItems.length; i += 1) {
 //   //   totalPrice += orderedItems[i];
@@ -43,3 +43,27 @@ pizzaPalace.order('Vienna', makePizza, onOrderError);
 //   return totalPrice;
 // }
 // calculateTotalPrice([164, 48, 291]);
+///////////////////////////////////////////////////////////
+const historyService = {
+  orders: [
+    { email: 'jacob@hotmail.com', dish: 'Burrito' },
+    { email: 'solomon@topmail.net', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'solomon@topmail.net', dish: 'Apple pie' },
+    { email: 'jacob@hotmail.com', dish: 'Taco' },
+  ],
+
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(' - ');
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+};
